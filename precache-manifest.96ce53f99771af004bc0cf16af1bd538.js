9dc5690b6534bbfd3102ecc4aaca4ec67895b4ab
bhprtk <bhprtk@gmail.com>
2020-12-12 13:49:43 -0800

    first commit

---
+++ precache-manifest.96ce53f99771af004bc0cf16af1bd538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa248dfd28f7136f3dc493d00d040bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a15266a04b1b0774270e",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bbdb05ba56706d2828f4",
+    "url": "/static/css/main.e6be4284.chunk.css"
+  },
+  {
+    "revision": "a15266a04b1b0774270e",
+    "url": "/static/js/2.5ddd4065.chunk.js"
+  },
+  {
+    "revision": "5218e592ced155baa53d790317e9631c",
+    "url": "/static/js/2.5ddd4065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdb05ba56706d2828f4",
+    "url": "/static/js/main.06ccf702.chunk.js"
+  },
+  {
+    "revision": "9dd1f3c04df7db97b051",
+    "url": "/static/js/runtime-main.4605c665.js"
+  }
+]);
